b4d9c4e280e029160b9d03958421186e030b8522
Mahdi Pablo <61613312+mahdipablo@users.noreply.github.com>
2025-04-05 18:51:29 +0330

    Update videoAds.js

--- src/pages/videoAds.js
+++ src/pages/videoAds.js
@@ -11,6 +11,6 @@ export function render() {
 export function afterRender() {
   document.getElementById("openLinkBtn").addEventListener("click", () => {
     // لینک صفحه ساده‌ای که می‌خوای باز شه
-    window.open("https://example.com/simple-page", "_blank");
+    window.open("https://testc-6b6.pages.dev/surf-ad", "_blank");
   });
 }
